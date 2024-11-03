b7abee177bfb4a8f9ab7e348c7161165556f06fb
Mark Ciril <markciril.jamon@gmail.com>
2024-11-03 14:55:05 +0800

    Chore: Update the route
    Remove the testins database abd functions

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\StatusController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('products', ProductController::class);
+Route::apiResource('status', StatusController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
