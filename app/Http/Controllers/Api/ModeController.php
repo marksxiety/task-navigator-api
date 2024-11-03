288d31072345c302409f6f858d71189bf3f8f0a1
Mark Ciril <markciril.jamon@gmail.com>
2024-11-03 16:30:17 +0800

    Feat: complete mode CRUD functions

--- app/Http/Controllers/Api/ModeController.php
+++ app/Http/Controllers/Api/ModeController.php
@@ -1,65 +1,92 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Mode;
+use App\Http\Resources\ModeResource;
+use Illuminate\Support\Facades\Validator;
 
 class ModeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $status = Mode::all();
+        return $status->isEmpty()
+            ? response()->json(['message' => 'No data available'], 200)
+            : ModeResource::collection($status);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255'
+        ]);
+    
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Invalid Request',
+                'error' => $validator->messages()
+            ], 422);
+        }
+    
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+    
+        $mode = Mode::create([
+            'name' => $request->name,
+        ]);
+    
+        return response()->json([
+            'message' => 'Created Successfully',
+            'data' => new ModeResource($mode)
+        ], 201);
+    }
+    
+    
+
+    public function show(Mode $mode)
+    {
+        return new ModeResource($mode);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+
+    public function update(Request $request, Mode $mode)
     {
-        //
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|string|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Invalid Request',
+                'error' => $validator->messages()
+            ], 422);
+        }
+    
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $mode->update([
+            'name' => $request->name,
+        ]);
+    
+        return response()->json([
+            'message' => 'Created Successfully',
+            'data' => new ModeResource($mode)
+        ], 201);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function destroy(Mode $mode)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $mode->delete();
+        return response()->json([
+            'message' => 'Deleted Successfully',
+            'data' => new ModeResource( $mode)
+        ],200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\StatusController;
+use App\Http\Controllers\Api\ModeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('status', StatusController::class);
+Route::apiResource('mode', ModeController::class);
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
