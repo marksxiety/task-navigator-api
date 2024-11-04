44e2b7986117e30779065c88d4548cead2293376
Mark Ciril <markciril.jamon@gmail.com>
2024-11-04 22:38:59 +0800

    Feat: Implement Sanctum authentication for API routes

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,24 @@ use App\Http\Controllers\Api\StatusController;
 use App\Http\Controllers\Api\ModeController;
 use App\Http\Controllers\Api\SystemController;
 use App\Http\Controllers\Api\TaskController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('status', StatusController::class);
-Route::apiResource('mode', ModeController::class);
-Route::apiResource('system', SystemController::class);
-Route::apiResource('task', TaskController::class);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function() {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/profile', [AuthController::class, 'profile']);
+
+    Route::apiResource('status', StatusController::class);
+    Route::apiResource('mode', ModeController::class);
+    Route::apiResource('system', SystemController::class);
+    Route::apiResource('task', TaskController::class);
+});
+
+
+
+
