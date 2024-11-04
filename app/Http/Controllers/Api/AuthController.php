73705e2546fabf3217c737485c30d09ab8d62a5c
Mark Ciril <markciril.jamon@gmail.com>
2024-11-04 22:37:03 +0800

    Feat: Authentication process for registration, login and logout

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,16 +6,95 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function profile(Request $request) {
+
+        if ($request->user()) {
+            return response()->json([
+                'message' => 'Profile data fetched',
+                'data' => $request->user()
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Invalid Request',
+            ], 500);
+        }
+
+    }
+
     public function register(Request $request) {
-        return 'reg';
+        
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users,email',
+            'password' => 'required|string|min:8|max:255',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        if ($user) {
+            $token = $user->createToken($user->name, ['auth-token'])->plainTextToken;
+
+            return response()->json([
+                'message' => 'Registration Successful',
+                'token_type' => 'Bearer',
+                'token' => $token
+            ], 201);
+        } else {
+            return response()->json([
+                'message' => 'Registration unsuccessful',
+            ], 500);
+        }
+
     }
     public function login(Request $request) {
-        return 'in';
+        
+        $request->validate([
+            'email' => 'required|email|max:255',
+            'password' => 'required|string|min:8|max:255',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Provided credentials are invalid'
+            ], 401);
+        }
+
+        $token = $user->createToken($user->name, ['auth-token'])->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login Successful',
+            'token_type' => 'Bearer',
+            'token' => $token
+        ], 200);
+
     }
     public function logout(Request $request) {
-        return 'out';
+
+        $user = User::where('id', $request->id)->first();
+
+        if ($user) {
+            $user->tokens()->delete();
+
+            return response()->json([
+                'message' => 'Logout Successfull',
+            ], 200);
+
+        } else {
+
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);
+
+        }
     }
 }
