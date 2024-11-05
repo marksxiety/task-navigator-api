3a2dd819739901450e78b9b016b3d79bd2aa6ba8
Mark Ciril <markciril.jamon@gmail.com>
2024-11-05 23:18:25 +0800

    Chore: Remove unnecessary 'use Request' statement from routes

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\Api\ModeController;
 use App\Http\Controllers\Api\SystemController;
 use App\Http\Controllers\Api\TaskController;
 use App\Http\Controllers\Api\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
